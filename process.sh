75539f63ca0ecabba67b017f2d0a4c442ad5b197
Ilya Rodionov <ris58h@gmail.com>
2018-09-19 17:42:57 +0300

    Update shell scripts.

--- process.sh
+++ process.sh
@@ -4,7 +4,8 @@ dir=test/process/$video_id
 rm -rf $dir
 mkdir -p $dir
 ./ytb-dl-frames.sh $video_id $dir "-gray"
-./contrast-dir.sh $dir "-gray" "-contrast"
-./negate-dir.sh $dir "-contrast" "-contrast-neg"
-./ocr-dir.sh $dir "-contrast" "-contrast"
-./ocr-dir.sh $dir "-contrast-neg" "-contrast-neg"
+./threshold-dir.sh $dir "-gray" "-monochrome"
+./negate-dir.sh $dir "-monochrome" "-monochrome-neg"
+./ocr-dir.sh $dir "-gray" "-gray"
+./ocr-dir.sh $dir "-monochrome" "-monochrome"
+./ocr-dir.sh $dir "-monochrome-neg" "-monochrome-neg"
