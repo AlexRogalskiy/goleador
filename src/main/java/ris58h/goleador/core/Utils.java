ec0de4bb7617102515ab9160b666b57905d3a4dc
Ilya Rodionov <ris58h@gmail.com>
2018-09-22 23:14:04 +0300

    Use BufferedInputStream in Utils.readInputToString.

--- src/main/java/ris58h/goleador/core/Utils.java
+++ src/main/java/ris58h/goleador/core/Utils.java
@@ -1,10 +1,12 @@
 package ris58h.goleador.core;
 
+import java.io.BufferedInputStream;
 import java.io.InputStream;
+import java.util.Scanner;
 
 public class Utils {
     public static String readInputToString(InputStream is) {
-        try (java.util.Scanner s = new java.util.Scanner(is)) {
+        try (Scanner s = new Scanner(new BufferedInputStream(is))) {
             return s.useDelimiter("\\A").hasNext() ? s.next() : "";
         }
     }
