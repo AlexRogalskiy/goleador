e175af02fb75749d6f92e26c360b1c35211da8ee
Ilya Rodionov <ris58h@gmail.com>
2018-09-22 19:05:30 +0300

    Change frames suffix to '-gray'.

--- src/main/java/ris58h/goleador/core/MainProcessor.java
+++ src/main/java/ris58h/goleador/core/MainProcessor.java
@@ -2,8 +2,8 @@ package ris58h.goleador.core;
 
 public class MainProcessor {
     public static void process(String videoId, String dirName) throws Exception {
-        FramesProcessor.process(videoId, dirName, "-monochrome");
-        PreOcrProcessor.process(dirName, "-monochrome", "-preocr");
+        FramesProcessor.process(videoId, dirName, "-gray");
+        PreOcrProcessor.process(dirName, "-gray", "-preocr");
         OcrProcessor.process(dirName, "-preocr", "-text");
         ScoresProcessor.process(dirName, "-text", "-score");
         TimestampsProcessor.process(dirName, "-score", "timestamps.txt");
