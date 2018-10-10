3ad91d6f73875a9813fc1da8499a2c42a8abf8a4
Ilya Rodionov <ris58h@gmail.com>
2018-10-10 23:39:04 +0300

    VideoUrlFetcher without youtube-dl.

--- worker/src/main/java/ris58h/goleador/worker/VideoUrlFetcher.java
+++ worker/src/main/java/ris58h/goleador/worker/VideoUrlFetcher.java
@@ -1,24 +1,75 @@
 package ris58h.goleador.worker;
 
-import java.util.concurrent.TimeUnit;
+import ris58h.goleador.processor.Utils;
 
-import static ris58h.goleador.processor.Utils.readInputToString;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLDecoder;
 
 public class VideoUrlFetcher {
 
+    private static final String FORMATS_KEY = "adaptive_fmts";
+
     public static String fetchFor(String videoId, String format) throws Exception {
-        System.out.println("Fetching video URL for videoId=" + videoId + " and format=" + format);
-        String command = "youtube-dl -f " + format + " -g https://www.youtube.com/watch?v=" + videoId;
-        Process process = Runtime.getRuntime().exec(command);
-        if (!process.waitFor(1, TimeUnit.MINUTES)) {
-            throw new RuntimeException("Process timeout");
+        //TODO https
+        URL url = new URL("http://www.youtube.com/get_video_info?video_id=" + videoId + "&el=embedded&ps=default&eurl=&gl=US&hl=en");
+        HttpURLConnection con = null;
+        String response;
+        try {
+            con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            response = Utils.readInputToString(con.getInputStream());
+        } finally {
+            if (con != null) {
+                con.disconnect();
+            }
         }
-        int exitCode = process.exitValue();
-        if (exitCode > 0) {
-            throw new RuntimeException(readInputToString(process.getErrorStream()));
+        return extractFromResponse(response, format);
+    }
+
+    static String extractFromResponse(String response, String format) throws Exception {
+        int formatsStart = response.indexOf(FORMATS_KEY + "=");
+        if (formatsStart >= 0) {
+            int formatsValueStart = formatsStart + FORMATS_KEY.length() + 1;
+            int formatsEnd = response.indexOf('&', formatsValueStart);
+            int formatsValueEnd = formatsEnd < 0 ? response.length() : formatsEnd;
+            String formatsEncodedValue = response.substring(formatsValueStart, formatsValueEnd);
+            String formatsValue = decode(formatsEncodedValue);
+            String[] entries = formatsValue.split(",");
+            for (String entry : entries) {
+                String[] subEntries = entry.split("&");
+                String url = null;
+                boolean formatFound = false;
+                for (String subEntry : subEntries) {
+                    String[] kv = subEntry.split("=");
+                    String key = kv[0];
+                    String rawValue = kv.length == 1 ? "" : kv[1];
+                    String value = decode(rawValue);
+                    if (key.equals("itag")) {
+                        if (value.equals(format)) {
+                            if (url != null) {
+                                return url;
+                            }
+                            formatFound = true;
+                        } else {
+                            break;
+                        }
+                    }
+                    if (key.equals("url")) {
+                        if (formatFound) {
+                            return value;
+                        } else {
+                            url = value;
+                        }
+                    }
+                }
+            }
         }
-        String videoUrl = readInputToString(process.getInputStream());
-        System.out.println("Video URL for videoId=" + videoId + " and format=" + format + ": " + videoUrl);
-        return videoUrl;
+        return null;
+    }
+
+    private static String decode(String value) throws UnsupportedEncodingException {
+        return URLDecoder.decode(value, "UTF-8");
     }
 }
---
+++ worker/src/test/java/ris58h/goleador/worker/VideoUrlFetcherTest.java
@@ -0,0 +1,20 @@
+package ris58h.goleador.worker;
+
+import org.junit.jupiter.api.Test;
+import ris58h.goleador.processor.Utils;
+
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VideoUrlFetcherTest {
+
+    @Test
+    void extractFromResponse() throws Exception {
+        InputStream is = VideoUrlFetcher.class.getResourceAsStream("/get_video_info_DU4Uij016HQ");
+        String response = Utils.readInputToString(is);
+        String videoUrl = VideoUrlFetcher.extractFromResponse(response, "136");
+        String expected = "https://r1---sn-axq7sn76.googlevideo.com/videoplayback?txp=5533432&ms=au%2Conr&itag=136&mt=1539095021&gir=yes&ip=94.19.239.167&requiressl=yes&ipbits=0&gcr=ru&mime=video%2Fmp4&id=o-AEZWFPNK2SZdysjOVxSOBENVc4T8LxA-c1Vui-V5XvNN&pl=20&mm=31%2C26&mn=sn-axq7sn76%2Csn-5goeen7y&fvip=1&keepalive=yes&mv=m&key=yt6&lmt=1538695792987922&c=WEB&signature=AB5A1728C14C77E8A3AFB4B66FD533ACB10AEDDB.BEF43CA88A145DC3CBA4302491DEAFE338ECD539&nh=IgphcjAyLmxlZDAzKgkxMjcuMC4wLjE%2C&source=youtube&clen=118273331&sparams=aitags%2Cclen%2Cdur%2Cei%2Cgcr%2Cgir%2Cid%2Cinitcwndbps%2Cip%2Cipbits%2Citag%2Ckeepalive%2Clmt%2Cmime%2Cmm%2Cmn%2Cms%2Cmv%2Cnh%2Cpl%2Crequiressl%2Csource%2Cexpire&aitags=133%2C134%2C135%2C136%2C160%2C242%2C243%2C244%2C247%2C278&expire=1539116714&ei=Srq8W_3FJJCd7QSGqIGABg&initcwndbps=1288750&dur=597.320";
+        assertEquals(expected, videoUrl);
+    }
+}
