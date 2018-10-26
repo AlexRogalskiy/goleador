20ddf130702f8f54087bbf903f39937080d150c2
Ilya Rodionov <ris58h@gmail.com>
2018-10-26 13:09:51 +0300

    Rename project.

--- commenter/entrypoint.sh
+++ commenter/entrypoint.sh
@@ -7,4 +7,4 @@ java \
   -Dgoogle.app.clientId=$GOOGLE_APP_CLIENT_ID \
   -Dgoogle.app.clientSecret=$GOOGLE_APP_CLIENT_SECRET \
   -Dgoogle.app.refreshToken=$GOOGLE_APP_REFRESH_TOKEN \
-  -jar /youtube-goleador-commenter-1.0-SNAPSHOT.jar
+  -jar /goleador-commenter-1.0-SNAPSHOT.jar
