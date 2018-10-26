6d2c563cf4edf18cfeb0c90306f3a3fe23673db0
Ilya Rodionov <ris58h@gmail.com>
2018-10-26 17:28:11 +0300

    Worker DataAccess.

--- worker/src/main/java/ris58h/goleador/worker/App.java
+++ worker/src/main/java/ris58h/goleador/worker/App.java
@@ -10,13 +10,10 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.sql.*;
 import java.util.List;
 import java.util.Optional;
 import java.util.Properties;
 import java.util.function.Function;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
 public class App {
     private static final Logger log = LoggerFactory.getLogger(App.class);
@@ -27,7 +24,7 @@ public class App {
     public static void main(String[] args) {
         Function<String, Optional<String>> appProperties = appProperties(args.length == 1 ? args[0] : null);
         long delay = appProperties.apply("worker.delay").map(Long::parseLong).orElse(DEFAULT_DELAY);
-        Supplier<Connection> connectionSupplier = connectionSupplier(
+        DataAccess dataAccess = new DataAccess(
                 appProperties.apply("datasource.url").get(),
                 appProperties.apply("datasource.username").get(),
                 appProperties.apply("datasource.password").get());
@@ -40,7 +37,7 @@ public class App {
         while (true) {
             String videoId = null;
             try {
-                videoId = fetchUnprocessedVideoId(connectionSupplier);
+                videoId = dataAccess.fetchUnprocessedVideoId();
             } catch (Exception e) {
                 log.error("Error while fetching unprocessed video: " + e.getMessage(), e);
             }
@@ -53,7 +50,7 @@ public class App {
                     List<Integer> times = process(videoId, tempDirectory, mainProcessor);
                     long elapsedTime = System.currentTimeMillis() - timeBefore;
                     log.info("Video " + videoId + " has been processed in " + (elapsedTime / 1000) + " seconds");
-                    updateVideoTimes(videoId, times, connectionSupplier);
+                    dataAccess.updateVideoTimes(videoId, times);
                     log.info("Video times have been updated");
                 } catch (Exception e) {
                     log.error("Processing error for " + videoId + " video: " + e.getMessage(), e);
@@ -62,7 +59,7 @@ public class App {
                         if (error == null) {
                             error = e.getClass().getName();
                         }
-                        updateError(videoId, error, connectionSupplier);
+                        dataAccess.updateError(videoId, error);
                     } catch (Exception ee) {
                         log.error("Updating error: " + ee.getMessage(), e);
                     }
@@ -115,67 +112,5 @@ public class App {
         String dirName = tempDirectory.toAbsolutePath().toString();
         List<ScoreFrames> scoreFrames = mainProcessor.process(videoUrl, dirName);
         return Highlighter.times(scoreFrames);
-    }
-
-    private static Supplier<Connection> connectionSupplier(String jdbcUrl, String username, String password) {
-        return () -> {
-            try {
-                return DriverManager.getConnection(jdbcUrl, username, password);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        };
-    }
-
-    private static String fetchUnprocessedVideoId(Supplier<Connection> connectionSupplier) throws Exception {
-        try (
-                Connection connection = connectionSupplier.get();
-                Statement statement = connection.createStatement();
-                ResultSet rs = statement.executeQuery(" SELECT video_id " +
-                        " FROM video " +
-                        " WHERE times IS NULL AND error IS NULL " +
-                        " LIMIT 1 ");
-        ) {
-            String videoId = rs.next() ? rs.getString(1) : null;
-            return videoId;
-        }
-    }
-
-    private static void updateVideoTimes(String videoId,
-                                         List<Integer> times,
-                                         Supplier<Connection> connectionSupplier) throws Exception {
-        String timeString = times.stream()
-                .map(Object::toString)
-                .collect(Collectors.joining(","));
-        try (
-                Connection connection = connectionSupplier.get();
-                PreparedStatement ps = connection.prepareStatement(" UPDATE video " +
-                        " SET times = ? " +
-                        " WHERE video_id = ? "
-                );
-        ) {
-            ps.setString(1, timeString);
-            ps.setString(2, videoId);
-            ps.executeUpdate();
-        }
-    }
-
-    private static void updateError(String videoId,
-                                        String error,
-                                        Supplier<Connection> connectionSupplier) throws Exception {
-        if (error.length() > 255) {
-            error = error.substring(0, 255);
-        }
-        try (
-                Connection connection = connectionSupplier.get();
-                PreparedStatement ps = connection.prepareStatement(" UPDATE video " +
-                        " SET error = ? " +
-                        " WHERE video_id = ? "
-                );
-        ) {
-            ps.setString(1, error);
-            ps.setString(2, videoId);
-            ps.executeUpdate();
-        }
     }
 }
