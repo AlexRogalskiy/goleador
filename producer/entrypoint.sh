958f1dc626f67c01d0b0e2010cb8eb679f5baad0
Ilya Rodionov <ris58h@gmail.com>
2018-11-07 18:10:51 +0300

    Delete 'producer' and 'commenter' projects.

--- producer/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-java \
-  -Ddatasource.url=$DATASOURCE_URL \
-  -Ddatasource.username=$DATASOURCE_USERNAME \
-  -Ddatasource.password=$DATASOURCE_PASSWORD \
-  -Dyoutube.apiKey=$YOUTUBE_API_KEY \
-  -jar /goleador-producer.jar
--- producer/src/main/java/ris58h/goleador/producer/Video.java
+++
@@ -1,9 +0,0 @@
-package ris58h.goleador.producer;
-
-public class Video {
-    public String id;
-    public Long duration;
-    public String definition;
-    public Long publishedAt;
-    public String title;
-}
