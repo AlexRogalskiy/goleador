a8b3bb1321c0a7266f121d68960111d3b6cd3366
Ilya Rodionov <ris58h@gmail.com>
2018-11-08 15:49:21 +0300

    Add shebang to build scripts.

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 cd db && source ./build.sh && cd .. \
 && cd main && source ./build.sh && cd .. \
 && cd worker && source ./build.sh && cd .. \
--- worker/build.sh
+++ worker/build.sh
@@ -1,1 +1,3 @@
+#!/usr/bin/env bash
+
 mvn clean package -Djavacpp.platform=linux-x86_64 && docker build -t ris58h/goleador-worker .
