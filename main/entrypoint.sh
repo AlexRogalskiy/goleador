bfabc2b358264dd907e804701daebf11a590f8c9
Ilya Rodionov <ris58h@gmail.com>
2018-11-13 19:14:12 +0300

    Update docker entrypoints.

--- main/entrypoint.sh
+++ main/entrypoint.sh
@@ -7,5 +7,6 @@ echo "youtube.apiKey=${YOUTUBE_API_KEY}" >> app.properties
 echo "google.app.clientId=${GOOGLE_APP_CLIENT_ID}" >> app.properties
 echo "google.app.clientSecret=${GOOGLE_APP_CLIENT_SECRET}" >> app.properties
 echo "google.app.refreshToken=${GOOGLE_APP_REFRESH_TOKEN}" >> app.properties
+echo "rabbitmq.uri=${RABBITMQ_URI}" >> app.properties
 
 java -jar /goleador-main.jar app.properties
--- worker/entrypoint.sh
+++ worker/entrypoint.sh
@@ -3,5 +3,6 @@
 echo "datasource.url=${DATASOURCE_URL}" >> app.properties
 echo "datasource.username=${DATASOURCE_USERNAME}" >> app.properties
 echo "datasource.password=${DATASOURCE_PASSWORD}" >> app.properties
+echo "rabbitmq.uri=${RABBITMQ_URI}" >> app.properties
 
 java -jar /goleador-worker.jar app.properties
